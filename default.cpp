6b3b7d12ec0dbeb0ebf048ab2469316eb9e1ab82
Shivam Singhal <44235074+ShivamSinghal1@users.noreply.github.com>
2019-09-30 10:04:14 +0530

    Create default.cpp

---
+++ default.cpp
@@ -0,0 +1,15 @@
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " : " << arg1 << std::endl;
+        //use cerr if u want to display at the bottom
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ','); cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+    }
+#else
+#define trace(...)
+#endif
