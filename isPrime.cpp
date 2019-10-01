79b3842a1511e7838fb9d7d0c246206ee2fc0f5e
Shivam Singhal <44235074+ShivamSinghal1@users.noreply.github.com>
2019-10-01 10:48:23 +0530

    Update isPrime.cpp

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,4 +1,4 @@
-bool isPrime(ll int n){
+bool isPrime(ll n){
     if(n<=1)
         return false;
     if(n==2)
@@ -6,8 +6,8 @@ bool isPrime(ll int n){
     if(n&1==0)
         return false;
         
-    ll int rt = sqrt(n);
-    for(ll int i=3;i<=rt;i+=2){
+    ll rt = sqrt(n);
+    for(ll i=3;i<=rt;i+=2){
         if(n%i==0)
             return false;
     }
