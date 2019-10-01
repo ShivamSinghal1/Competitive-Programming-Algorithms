720f576c90803589e9380b2178c9f009df6b8729
Shivam Singhal <44235074+ShivamSinghal1@users.noreply.github.com>
2019-10-01 10:52:29 +0530

    Create modInverse.cpp

---
+++ modInverse.cpp
@@ -0,0 +1,34 @@
+ll gcdExtended(ll a,ll b,ll &x,ll &y)
+{
+	// Base Case
+	if (a == 0)
+	{
+		x = 0, y = 1;
+		return b;
+	}
+
+	ll x1, y1; // To store results of recursive call
+	ll gcd = gcdExtended(b%a, a, x1, y1);
+
+	// Update x and y using results of recursive
+	// call
+	x = y1 - (b/a) * x1;
+	y = x1;
+
+	return gcd;
+}
+
+ll modInverse(ll a,ll m)
+{
+	ll x, y;
+	ll g = gcdExtended(a, m, x, y);
+	if (g != 1)
+		cout << "Inverse doesn't exist";
+	else
+	{
+		// m is added to handle negative x
+		ll res = (x%m + m) % m;
+        return res;
+	}
+}
+
