4699ee24577b9ae92acb4fb98440667e0d4e6a25
Shashank-Chugh <42238022+Shashank-Chugh@users.noreply.github.com>
2019-10-01 10:25:37 +0530

    Create fio.cpp

---
+++ fio.cpp
@@ -0,0 +1,12 @@
+void fio()
+{
+   #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("ooo.txt", "w", stdout);
+    #endif
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+ 
+}
