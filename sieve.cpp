1e17c771b16015c8766857efcd61a5c22fc6958a
Shivam Singhal <44235074+ShivamSinghal1@users.noreply.github.com>
2019-10-04 12:00:09 +0530

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -1,17 +1,18 @@
 vector<ll> sieve(ll n,ll int*a)
 {
     vector<ll int>v;
-    fill(a,a+n,1);
-    a[0]=a[1]=0;
+    fill(a,a+n,0);
     ll i;
     rep(i,2,n+1)
     {
-        if(a[i]==0)
+        if(a[i]>0)
             continue;
+        a[i] = 1;
         ll j=i*i;
         while(j<=n)
         {
-            a[j]=0;
+            if(a[j]==0)
+                a[j]=i;
             j+=i;
         }
         v.pb(i); 
