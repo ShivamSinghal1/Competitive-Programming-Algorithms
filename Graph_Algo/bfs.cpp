dba36b9baad4f4284738a9d926e494f4e3ae52db
Shivam Singhal <44235074+ShivamSinghal1@users.noreply.github.com>
2019-12-25 14:34:00 +0530

    Create bfs.cpp

---
+++ Graph_Algo/bfs.cpp
@@ -0,0 +1,21 @@
+void graph::bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    isVisited[source] = 0;
+    while(!q.empty())
+    {
+        int parent = q.front();
+        cout<<parent+1<<" ";
+        q.pop();
+        int child;
+        rep(child,0,adj_list[parent].size())
+        {
+            if(isVisited[adj_list[parent][child]]==-1)
+            {
+                q.push(adj_list[parent][child]);
+                isVisited[adj_list[parent][child]] = isVisited[parent] + 1;
+            }
+        }
+    }
+}
