de510929d62189a15f905f4784f4dc9f0eed9bf8
Shivam Singhal <44235074+ShivamSinghal1@users.noreply.github.com>
2019-12-25 14:32:59 +0530

    Update dfs.cpp

--- Graph_Algo/dfs.cpp
+++ Graph_Algo/dfs.cpp
@@ -4,6 +4,6 @@ void graph::dfs(int source)
     cout<<source+1<<" "; 
     int i;
     rep(i,0,adj_list[source].size())
-        if(isVisited[adj_list[source][i]]==0)
+        if(isVisited[adj_list[source][i]]==-1)
             dfs(adj_list[source][i]);
 }
