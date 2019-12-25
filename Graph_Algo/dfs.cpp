59d80830a85c6d5a2ae5c62a098893bec0f59b8d
Shivam Singhal <44235074+ShivamSinghal1@users.noreply.github.com>
2019-12-25 14:21:12 +0530

    Update dfs.cpp

--- Graph_Algo/dfs.cpp
+++ Graph_Algo/dfs.cpp
@@ -1,38 +1,9 @@
-class graph
+void graph::dfs(int source)
 {
-    public: 
-        int n_v,n_e;
-        vector<vector<int>> adj_list;
-        bool isDirected;
-
-        void input(bool isDir=true)
-        {
-            cin>>n_v>>n_e;
-            adj_list.resize(n_v);
-            isDirected = isDir;
-            int to,fro;
-            for(int i=0;i<n_e;++i){
-                cin>>to>>fro;
-                addEdge(to,fro,isDir);
-            }
-        }
-
-        void addEdge(int to,int fro,bool isDir=true)
-        {
-            adj_list[to].push_back(fro);
-            if(isDir)
-                adj_list[fro].push_back(to);
-        }
-
-}
-
-void dfs(graph &g, bool* isVisited, int source)
-{
-    isVisited[source] = true;
-    // cout<<source+1<<" "; 
-
+    isVisited[source] = 1;
+    cout<<source+1<<" "; 
     int i;
-    rep(i,0,g.adj_list[source].size())
-        if(!isVisited[g.adj_list[source][i]])
-            dfs(g,isVisited,g.adj_list[source][i]);
+    rep(i,0,adj_list[source].size())
+        if(isVisited[adj_list[source][i]]==0)
+            dfs(adj_list[source][i]);
 }
