b610e94c03191c2bd5a66466d1b253828b86dbdd
Shivam Singhal <44235074+ShivamSinghal1@users.noreply.github.com>
2020-06-20 23:18:58 +0530

    Update isPrime.cpp

--- Mathematics_Algo/isPrime.cpp
+++ Mathematics_Algo/isPrime.cpp
@@ -1,13 +1,11 @@
-bool isPrime(ll n){
+bool isPrime(int n){
     if(n<=1)
         return false;
     if(n==2)
         return true;
     if(n%2==0)
         return false;
-        
-    ll rt = sqrt(n);
-    for(ll i=3;i<=rt;i+=2){
+    for(int i=3;i*i<=n;i+=2){
         if(n%i==0)
             return false;
     }
