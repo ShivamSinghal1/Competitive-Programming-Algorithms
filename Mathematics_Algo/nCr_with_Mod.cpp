678982507fc814ae7e63671d641857cdd89cdd1e
Shivam Singhal <44235074+ShivamSinghal1@users.noreply.github.com>
2020-08-13 12:10:07 +0530

    Update nCr_with_Mod.cpp

--- Mathematics_Algo/nCr_with_Mod.cpp
+++ Mathematics_Algo/nCr_with_Mod.cpp
@@ -1,5 +1,5 @@
-ll powMod(ll n,ll p,ll m){
-    ll res=1;
+int powMod(int n,int p,int m){
+    int res=1;
     n%=m;
     while(p>0){
         if(p&1)
@@ -11,26 +11,26 @@ ll powMod(ll n,ll p,ll m){
 }
 
 
-const ll MAX = 1<<20;
-ll fact[MAX];
-ll ifact[MAX];
-void preCalcFac(ll m){
+const int MAX = 1<<20;
+int fact[MAX];
+int ifact[MAX];
+void preCalcFac(int m){
     fact[0] = 1;
-    for(ll i = 1; i < MAX; ++i)
+    for(int i = 1; i < MAX; ++i)
         fact[i] = (fact[i - 1] * i) % m;
 
     ifact[MAX - 1] = powMod(fact[MAX - 1], m - 2, m);
-    for(ll i = MAX-2; i >= 0 ; --i)
+    for(int i = MAX-2; i >= 0 ; --i)
         ifact[i] = (ifact[i + 1] * (i + 1)) % m;
 }
 
 
-ll nCr(ll n,ll r,ll m) 
+int nCr(int n,int r,int m) 
 { 
    // Base case 
    if (r==0) 
       return 1;
-    ll ans = (fact[n]*ifact[r])%m;
+    int ans = (fact[n]*ifact[r])%m;
     ans = (ans*ifact[n-r])%m;
     return ans;
 }
