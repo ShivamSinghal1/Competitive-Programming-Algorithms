673d9573f64cece1db59f96d75981531a6a90374
David Kanekanian <dkanekanian1@gmail.com>
2020-09-25 15:49:58 +0100

    Add sorting by splitting to groups and combine

--- Sorting_Algo/merge_sort.cpp
+++ Sorting_Algo/merge_sort.cpp
@@ -9,6 +9,7 @@
 
 template<class T>
 std::vector<T> merge_sort(const std::vector<T>& to_sort) {
+    // Merge two sorted arrays component.
     auto merge_ = [](const std::vector<T>& a, const std::vector<T>& b) {
         std::vector<T> ret;
         std::size_t i{0}, j{0};
@@ -22,9 +23,27 @@ std::vector<T> merge_sort(const std::vector<T>& to_sort) {
         // Return merged array.
         return ret;
     };
-    for (auto el: merge_({ 1, 2}, {3, 4})) std::cout<<el <<' ';
-return {};
 
+    // Turn to_sort into groups of one.
+    std::vector<std::vector<T>> groups;
+    groups.reserve(to_sort.size());
+    std::transform(
+        begin(to_sort), end(to_sort), std::back_inserter(groups),
+        [](const auto& el) { return std::vector<T>{ {el} }; }
+    );
+
+    while(groups.size() > 1) {
+        // Counters needed to be declared in this scope.
+        std::size_t i{0}, j{0};
+        for (; i + 1 < groups.size(); i += 2, j++) {
+            // Merge pairs of groups and store at the beginning.
+            groups[j] = merge_(groups[i], groups[i + 1]);
+        }
+        // Remove merged groups.
+        groups.erase(begin(groups) + j, end(groups) - (groups.size() % 2));
+    }
+
+    return groups[0];
 }
 
 int main() {
