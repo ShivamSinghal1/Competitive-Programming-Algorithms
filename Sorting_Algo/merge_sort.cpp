8f71665bf02925fe2f8306d92521ae075b598e26
David Kanekanian <dkanekanian1@gmail.com>
2020-09-25 18:43:08 +0100

    Add comparison function by template type

--- Sorting_Algo/merge_sort.cpp
+++ Sorting_Algo/merge_sort.cpp
@@ -7,15 +7,15 @@
 #include <iostream>
 
 
-template<class T>
-std::vector<T> merge_sort(const std::vector<T>& to_sort) {
+template<class T, class Compare>
+std::vector<T> merge_sort(const std::vector<T>& to_sort, Compare cmp) {
     // Merge two sorted arrays component.
-    auto merge_ = [](const std::vector<T>& a, const std::vector<T>& b, std::vector<T>& out) {
+    auto merge_ = [&cmp](const std::vector<T>& a, const std::vector<T>& b, std::vector<T>& out) {
         std::size_t i{0}, j{0};
         std::size_t size_a{a.size()}, size_b{b.size()};
         while (i < size_a && j < size_b) {
             // Add best element between both arrays.
-            out.push_back(a[i] < b[j] ? a[i++] : b[j++]);
+            out.push_back(cmp(a[i], b[j]) ? a[i++] : b[j++]);
         }
         // Add remaining elements.
         while (i < size_a) { out.push_back(a[i++]); }
@@ -48,7 +48,7 @@ std::vector<T> merge_sort(const std::vector<T>& to_sort) {
 
 int main() {
     std::vector<int> v{5, 3, 6, 2, 7};
-    auto v1 = merge_sort(v);
+    auto v1 = merge_sort(v, [](auto a, auto b){ return a < b; });
     for (const auto& el : v1) {
         std::cout << el << " ";
     }
