07255521cec844d8a532ec066bcfcaa7e70bb9e3
David Kanekanian <dkanekanian1@gmail.com>
2020-09-25 20:37:04 +0100

    Clarify variable names in merge function

--- Sorting_Algo/merge_sort.cpp
+++ Sorting_Algo/merge_sort.cpp
@@ -13,15 +13,15 @@ template<class T, class Compare>
 std::vector<T> merge_sort(const std::vector<T>& to_sort, Compare cmp) {
     // Merge two sorted arrays component.
     auto merge_ = [&cmp](const std::vector<T>& a, const std::vector<T>& b, std::vector<T>& out) {
-        std::size_t i{0}, j{0};
-        std::size_t size_a{a.size()}, size_b{b.size()};
-        while (i < size_a && j < size_b) {
+        std::size_t a_index{ 0 }, b_index{ 0 };
+        std::size_t a_size{ a.size() }, b_size{ b.size() };
+        while (a_index < a_size && b_index < b_size) {
             // Add best element between both arrays.
-            out.push_back(cmp(a[i], b[j]) ? a[i++] : b[j++]);
+            out.push_back(cmp(a[a_index], b[b_index]) ? a[a_index++] : b[b_index++]);
         }
         // Add remaining elements.
-        while (i < size_a) { out.push_back(a[i++]); }
-        while (j < size_b) { out.push_back(b[j++]); }
+        while (a_index < a_size) { out.push_back(a[a_index++]); }
+        while (b_index < b_size) { out.push_back(b[b_index++]); }
     };
 
     // Turn to_sort into groups of one.
@@ -49,15 +49,16 @@ std::vector<T> merge_sort(const std::vector<T>& to_sort, Compare cmp) {
 }
 
 // Demo
-// Demo requires at least C++17
-// #include <iostream>
-//
-// int main() {
-//     std::vector v{5, 3, 6, 2, 7};
-//     auto v1 = merge_sort(v, [](auto a, auto b){ return a < b; });
-//     for (const auto& el : v1) {
-//         std::cout << el << " ";
-//     }
-//     std::cout << '\n';
-// .   // Output: 2 3 5 6 7
-// }
+/*
+#include <iostream>
+
+int main() {
+    std::vector v{5, 3, 6, 2, 7};
+    auto v1 = merge_sort(v, [](auto a, auto b){ return a < b; });
+    for (const auto& el : v1) {
+        std::cout << el << " ";
+    }
+    std::cout << '\n';
+    // Output: 2 3 5 6 7
+}
+*/
