93bd8b9fcac3be43930f4408733055d3bd60b1d6
David Kanekanian <dkanekanian1@gmail.com>
2020-09-25 20:23:12 +0100

    Revert "Add iterator based merge two sorted arrays function"
    This reverts commit 2fde46a06ffc2f31151adfc258e1b18e11a8da2d.

--- Sorting_Algo/merge_sort.cpp
+++ Sorting_Algo/merge_sort.cpp
@@ -9,29 +9,16 @@
 template<class T, class Compare>
 std::vector<T> merge_sort(const std::vector<T>& to_sort, Compare cmp) {
     // Merge two sorted arrays component.
-    auto merge_ = [&cmp](
-        auto a_first, auto a_last,
-        auto b_first, auto b_last, auto ret_first
-    ) {
-        auto a = a_first;
-        auto b = b_first;
-        auto ret = ret_first;
-        const auto a_len = static_cast<std::size_t>(std::distance(a_first, a_last));
-        const auto b_len = static_cast<std::size_t>(std::distance(b_first, b_last));
-        while (a < a_last && b < b_last) {
+    auto merge_ = [&cmp](const std::vector<T>& a, const std::vector<T>& b, std::vector<T>& out) {
+        std::size_t i{0}, j{0};
+        std::size_t size_a{a.size()}, size_b{b.size()};
+        while (i < size_a && j < size_b) {
             // Add best element between both arrays.
-            if (cmp(*a, *b)) {
-                *ret = *a;
-                ++a;
-            } else {
-                *ret = *b;
-                ++b;
-            }
-            ++ret;
+            out.push_back(cmp(a[i], b[j]) ? a[i++] : b[j++]);
         }
         // Add remaining elements.
-        while (a < a_last) { *ret = *a; ++a; ++ret; }
-        while (b < b_last) { *ret = *b; ++b; ++ret; }
+        while (i < size_a) { out.push_back(a[i++]); }
+        while (j < size_b) { out.push_back(b[j++]); }
     };
 
     // Turn to_sort into groups of one.
@@ -48,9 +35,7 @@ std::vector<T> merge_sort(const std::vector<T>& to_sort, Compare cmp) {
         for (; i + 1 < groups.size(); i += 2, j++) {
             // Merge pairs of groups and store at the beginning.
             const auto len = groups[j].size();
-            merge_(begin(groups[i]), end(groups[i]),
-                   begin(groups[i + 1]), end(groups[i + 1]),
-                   std::back_inserter(groups[j]));
+            merge_(groups[i], groups[i + 1], groups[j]);
             groups[j].erase(begin(groups[j]), begin(groups[j]) + len);
         }
         // Remove merged groups.
@@ -61,14 +46,14 @@ std::vector<T> merge_sort(const std::vector<T>& to_sort, Compare cmp) {
 }
 
 // Demo
-#include <iostream>
-
-int main() {
-    std::vector<int> v{5, 3, 6, 2, 7};
-    auto v1 = merge_sort(v, [](auto a, auto b){ return a < b; });
-    for (const auto& el : v1) {
-        std::cout << el << " ";
-    }
-    std::cout << '\n';
-}
+// #include <iostream>
+// 
+// int main() {
+//     std::vector<int> v{5, 3, 6, 2, 7};
+//     auto v1 = merge_sort(v, [](auto a, auto b){ return a < b; });
+//     for (const auto& el : v1) {
+//         std::cout << el << " ";
+//     }
+//     std::cout << '\n';
+// }
 
