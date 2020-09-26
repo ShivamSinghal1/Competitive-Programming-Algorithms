2da299906e18f696bbb1e5862c63717611006811
David Kanekanian <dkanekanian1@gmail.com>
2020-09-26 12:06:59 +0100

    FOrmat file for merge sort vector

--- Sorting_Algo/merge_sort.cpp
+++ Sorting_Algo/merge_sort.cpp
@@ -1,7 +1,7 @@
 // Merge Sort Algorithm
 // Author: David Kanekanian
-// This implementation requires at least C++11 due to lambda.
-// But the demo requires at least C++17 due to template deductions.
+// This implementation requires at least C++11.
+// But the demo below requires at least C++17.
 
 
 #include <algorithm>  // std::transform
@@ -14,7 +14,7 @@ std::vector<T> merge_sort(const std::vector<T>& to_sort, Compare cmp) {
     // Merge two sorted arrays component.
     auto merge_ = [&cmp](const std::vector<T>& a, const std::vector<T>& b, std::vector<T>& out) {
         std::size_t a_index{ 0 }, b_index{ 0 };
-        std::size_t a_size{ a.size() }, b_size{ b.size() };
+        const std::size_t a_size{ a.size() }, b_size{ b.size() };
         while (a_index < a_size && b_index < b_size) {
             // Add best element between both arrays.
             out.push_back(cmp(a[a_index], b[b_index]) ? a[a_index++] : b[b_index++]);
@@ -34,7 +34,7 @@ std::vector<T> merge_sort(const std::vector<T>& to_sort, Compare cmp) {
 
     while (groups.size() > 1) {
         // Counters needed to be declared in this scope.
-        std::size_t i{0}, j{0};
+        std::size_t i{ 0 }, j{ 0 };
         for (; i + 1 < groups.size(); i += 2, j++) {
             // Merge pairs of groups and store at the beginning.
             const auto len = groups[j].size();
@@ -53,7 +53,7 @@ std::vector<T> merge_sort(const std::vector<T>& to_sort, Compare cmp) {
 #include <iostream>
 
 int main() {
-    std::vector v{5, 3, 6, 2, 7};
+    std::vector v{ 5, 3, 6, 2, 7 };
     auto v1 = merge_sort(v, [](auto a, auto b){ return a < b; });
     for (const auto& el : v1) {
         std::cout << el << " ";
